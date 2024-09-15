73014a448a19ebe7f59d07cbea0186a14647e200
nirbhay-design <sharma.59@iitj.ac.in>
2024-09-16 01:30:27 +0530

    adding DSA assignment 1 initial commit

---
+++ DSA/heaps.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std; 
+
+class maxHeap{
+    maxHeap () {
+        int heap[1000];
+    }
+
+    void maxHeapify(int index) {
+
+    }
+
+    void buildMaxHeap() {
+
+    }
+
+    int extractMaxElement() {
+
+    }
+
+    void deleteFromMaxHeap() {
+
+    }
+
+    void insert(int element) {
+
+    }
+
+    void heapSort() {
+
+    }
+};
+
+
+int main () {
+    return 0;
+}
