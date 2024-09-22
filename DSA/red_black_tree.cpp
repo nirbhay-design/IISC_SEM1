17281c4d452d46eddeb2ce89b44b5d7dcf5af9d7
nirbhay-design <sharma.59@iitj.ac.in>
2024-09-22 17:16:08 +0530

    red black tree insertion done

--- DSA/red_black_tree.cpp
+++ DSA/red_black_tree.cpp
@@ -2,8 +2,8 @@
 
 using namespace std; 
 
-#define B 1;
-#define R 0;
+#define B 1
+#define R 0
 
 class node{
     public:
@@ -48,6 +48,45 @@ class RBTree {
         return _search(temp, x);
     }
 
+    void _rb_insert_fixup(node * z) {
+        while (z != NULL && z->p != NULL && z->p->c == R) {
+            if (z->p == z->p->p->l) {
+                node * y = z->p->p->r;
+                if (y != NULL && y->c == R) {
+                    z->p->c = B;
+                    z->p->p->c = R;
+                    if (y != NULL) y->c = B;
+                    z = z->p->p;
+                } else {
+                    if (z == z->p->r) {
+                        z = z->p;
+                        leftRotate(z);
+                    }
+                    z->p->c = B;
+                    z->p->p->c = R;
+                    rightRotate(z->p->p);
+                }
+            } else {
+                node * y = z->p->p->l;
+                if (y != NULL && y->c == R) {
+                    z->p->c = B;
+                    z->p->p->c = R;
+                    if (y != NULL) y->c = B;
+                    z = z->p->p;
+                } else {
+                    if (z == z->p->l) {
+                        z = z->p;
+                        rightRotate(z);
+                    }
+                    z->p->c = B;
+                    z->p->p->c = R;
+                    leftRotate(z->p->p);
+                }
+            }   
+        }
+        root->c = B;
+    }
+
     void insert(int e) {
         node * temp = root;
         node * new_node = new node(e);
@@ -55,6 +94,7 @@ class RBTree {
         if (temp == NULL) { // root is null
             root = new_node;
             root->c = B;
+            return;
         }
 
         while (temp != NULL) {
@@ -75,20 +115,65 @@ class RBTree {
                 temp = temp->r;
             }
         }
-
+        if (new_node->p->c == R) { 
+            _rb_insert_fixup(new_node);
+        }
     }
 
     void leftRotate(node * x) {
         node * y = x->r;
         node * yl = y->l;
+
         y->l = x;
         x->r = yl;
+
+        if (yl != NULL) {
+            yl->p = x;
+        }
+
+        if (x == root) {
+            root = y;
+        } else {
+            if (x == x->p->l) {
+                x->p->l = y;
+            } else {
+                x->p->r = y;
+            }
+        }
         
+        y->p = x->p;
+        x->p = y;
+    }
+
+    void rightRotate(node * x) {
+        node * y = x->l;
+        node * yr = y->r;
+
+        y->r = x;
+        x->l = yr;
+
+        if (yr != NULL) {
+            yr->p = x;
+        }
+
+        if (x == root) {
+            root = y;
+        } else {
+            if (x == x->p->l) {
+                x->p->l = y;
+            } else {
+                x->p->r = y;
+            }
+        }
+
+        y->p = x->p;
+        x->p = y;
+
     }
 
     void _print_tree(node * temp) {
         if (temp != NULL) {
-            cout << "(" << temp->d << " " << temp->c << ") ";
+            cout << "(" << temp->d << " " << (temp->c ? "B":"R") << ") ";
             _print_tree(temp->l);
             _print_tree(temp->r);
         }
@@ -97,17 +182,27 @@ class RBTree {
     void printTree() {
         node * temp = root;
         _print_tree(temp);
+        cout << endl;
     }
 };
 
 int main () {
     RBTree * rbtree = new RBTree();
     rbtree->insert(34);
-    rbtree->insert(12);
-    rbtree->insert(13);
-    rbtree->insert(16);
+    rbtree->printTree();
+    rbtree->insert(48);
+    rbtree->printTree();
+    rbtree->insert(43);
+    rbtree->printTree();
+    rbtree->insert(52);
+    rbtree->printTree();
     rbtree->insert(32);
+    rbtree->printTree();
     rbtree->insert(19);
+    rbtree->printTree();
+    rbtree->insert(33);
+    rbtree->printTree();
+    rbtree->insert(12);
     rbtree->printTree();
 
     return 0;
