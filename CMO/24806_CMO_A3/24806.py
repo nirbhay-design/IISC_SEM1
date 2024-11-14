ad081579624b4c2b83ca2e6434a7905cf8e38377
nirbhay-design <sharma.59@iitj.ac.in>
2024-11-14 20:52:56 +0530

    adding assignment

--- CMO/24806_CMO_A3/24806.py
+++ CMO/24806_CMO_A3/24806.py
@@ -1,7 +1,11 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
-import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import cvxpy as cp
+import os
+import warnings; warnings.filterwarnings("ignore")
+
+os.environ['CVXPY_DISABLE_DCP_CHECK'] = '1'
 
 def projection(A, b, z):
     # z and b both are (n, 1)
@@ -38,30 +42,50 @@ def read_csv(data, label):
         l = list(map(lambda x: float(x.replace("\n", "")), l))
     return np.array(d), np.array(l)
 
-def plot_points(data, label):
+def plot_points(data, label, w, b, ac):
+    color_map = {}
+
     for i, l in enumerate(label):
-        if l == 1:
-            plt.scatter(data[i,0], data[i,1], color='b')
+        if i in ac:
+            if l == 1:
+                plt.scatter(data[i,0], data[i,1], label='y=1', color='r')
+            else:
+                plt.scatter(data[i,0], data[i,1], label='y=-1', marker='s', color='r')
         else:
-            plt.scatter(data[i,0], data[i,1], marker='s', color='r')
+            if l == 1:
+                plt.scatter(data[i,0], data[i,1], label='y=1', color='b')
+            else:
+                plt.scatter(data[i,0], data[i,1], label='y=-1', marker='s', color='g')
+    x1 = np.arange(-3,3,0.1)
+    x2 = -(b + w[0] * x1) / w[1]
+    plt.plot(x1,x2, label='w^Tx + b', c = 'black')      
+    plt.xlabel("x1")
+    plt.ylabel("x2")      
+    # plt.legend()
     plt.show()
     
 def dual_solve(data, labels):
-    A = (np.outer(labels, labels)) * (data @ data.T)
+    A = (np.outer(labels, labels)) * (np.dot(data, data.T))
     N = data.shape[0]
-
     lambd = cp.Variable(N)
-    obj = cp.Maximize(cp.sum(lambd) - 0.5 * cp.quad_form(lambd, A))
+    obj = cp.Maximize(cp.sum(lambd) - 0.5 * cp.quad_form(lambd, A, assume_PSD=True))
     constraints = [
         lambd >= 0,
-        labels @ lambd == 0
+        cp.sum(labels * lambd) == 0
     ]
 
     problem = cp.Problem(obj, constraints)
     problem.solve()
 
     lambda_opt = lambd.value
-    print(lambda_opt)
+    lambda_opt[lambda_opt <= 1e-4] = 0
+
+    w = np.sum((lambda_opt * labels).reshape(-1,1) * data, axis=0)
+    b = labels[0] - np.dot(w, data[0])
+
+    dual_obj = np.sum(lambda_opt) - 0.5 * np.sum(lambda_opt.reshape(-1,1).T @ A @ lambda_opt.reshape(-1,1))
+
+    return w, b, lambda_opt, dual_obj
 
 def primal_solve(data, labels):
     N = data.shape[1]
@@ -73,7 +97,7 @@ def primal_solve(data, labels):
     ]
     prob = cp.Problem(objective, constrns)
     prob.solve()
-    print(w.value, b.value)
+    return w.value, b.value
 
 def que1():
     # AA = np.array([[2,-4,2,-14], [-1,2,-2,11],[-1,2,-1,7]])
@@ -83,7 +107,7 @@ def que1():
 
     x = min_norm_soln(A, b).reshape(-1)
 
-    print(f"least norm solution: x* = {x}, with norm: {np.linalg.norm(x)}")
+    print(f"least norm solution: x* = {x}, with norm: {np.linalg.norm(x):.5f}")
     # print(np.linalg.norm(AA @ x - bb))
 
     outs = {}
@@ -94,14 +118,31 @@ def que1():
     plot(outs)
 
 if __name__ == "__main__":
-    # que1()
-    data, label = read_csv('Data.csv', "Labels.csv")
+    ans = int(input("which question ans do you want [1/2]: "))
+    
+    if ans == 1:
+        que1()
+    else:
+        data, label = read_csv('Data.csv', "Labels.csv")
 
-    print(data)
-    print(label)
+        w, b = primal_solve(data, label)
+        print(f"PRIMAL: w = {w}, b = {b}, objective value = {0.5 * np.linalg.norm(w)**2:.4f}")
 
-    # primal_solve(data, label)
-    dual_solve(data, label)
+        wd, bd, lambd, dual_obj = dual_solve(data, label)
+        print(f"DUAL: w = {wd}, b = {bd}, objective value = {dual_obj:.4f}")
+
+        sum_lambd = [0, 0]
+        for i in range(len(lambd)):
+            if label[i] == 1:
+                sum_lambd[0] += lambd[i]
+            else:
+                sum_lambd[1] += lambd[i]
+        print(f"sum_lambda (y = 1): {sum_lambd[0]:.4f}, (y=-1): {sum_lambd[1]:.4f}")
+
+        active_constr = np.where(lambd > 0)[0]
+        print(f"active constraints are: {active_constr + 1}")
+
+        plot_points(data, label, wd, bd, active_constr)
 
 
     
